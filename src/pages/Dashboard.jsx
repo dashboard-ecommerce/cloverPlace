6e53320ad4a3bccfab64bf4f3f43fa1f6c0f9e01
Ma-shi22 <mashisantos@gmail.com>
2023-02-28 12:37:15 +0100

    add dasboard y editproduct

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "react-router-dom"
+import { useState } from "react";
+import { productHandler } from "../handlers/productHandler";
+import Product from '../components/Product'
+import './Dashboard.css'
+
+function Dashboard() {
+    const { products } = useLoaderData();
+    const [productsData, setProductsData] = useState(products);
+
+    const deleteProduct = async (id) => {
+      await productHandler.deleteProduct(id);
+      setProductsData(productsData.filter(product => product.id !== id))
+  }
+
+    return (
+        <>
+        {productsData.map((product) => (
+            <Product key={product.id} product={product} deleteProduct={deleteProduct} />
+        ))}
+      </>
+    )
+}
+
+export default Dashboard;
